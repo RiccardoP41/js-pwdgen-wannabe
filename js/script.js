b610da41fbbcaa4b36aae5b66af2e43bd99a568d
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-07-06 14:41:32 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ var cognome = prompt ("Bene! Ora dimmi il tuo cognome");
 
 var colore = prompt ("Ottimo! Qual è il colore che preferisci?");
 
-document.getElementById("pwd").innerHTML = "Complimenti! La tua password é" + " " + nome + cognome + colore + 41 + " " + "!";
+var numero = parseInt(prompt ("Qual è il tuo numero fortunato?"));
+
+
+document.getElementById("pwd").innerHTML = "Complimenti! La tua password é" + " " + nome + cognome + colore + numero + " " + "!";
